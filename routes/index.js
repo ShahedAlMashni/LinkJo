f11eb33a318e3e4d0a3b749a0e3713cba08ff532
ShahedAlMashni <shahedmashni@gmail.com>
2020-09-14 11:56:33 +0300

    add profile page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Express' });
+});
+
 module.exports = router;
